aa815c2d5f552107cb9869c833a585850728979e
Matheus Kenzo <100180936+matheuskya@users.noreply.github.com>
2024-09-12 15:54:52 -0300

    Projeto iniciado

---
+++ content/index.md
@@ -0,0 +1,18 @@
+Title: Welcome to My Resume
+Date: 2024-09-12
+
+## Connect with me
+- [Github](https://github.com/matheuskya)
+- [LinkedIn](https://www.linkedin.com/in/matheus-kenzo-614a4120b/)
+
+
+## Skills
+
+### Programming
+- Web Development: Django, FastAPI, ASP.NET
+- Databases: PostgreSQL, MySQL
+
+### Languages
+- Python
+- C#
+- Go
---
+++ pelicanconf.py
@@ -0,0 +1,40 @@
+AUTHOR = 'Matheus Kenzo'
+SITENAME = 'Matheus Kenzo'
+SITEURL = ""
+
+DISPLAY_PAGES_ON_MENU = False
+DISPLAY_CATEGORIES_ON_MENU = False
+THEME="notmyidea"
+
+PATH = "content"
+
+TIMEZONE = 'America/Sao_Paulo'
+
+DEFAULT_LANG = 'pt'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("You can modify those links in your config file", "#"),
+)
+
+# Social widget
+SOCIAL = (
+    ("Github", "https://github.com/matheuskya"),
+    ("Linkedin", "https://www.linkedin.com/in/matheus-kenzo-614a4120b/"),
+)
+
+DEFAULT_PAGINATION = 1
+
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
